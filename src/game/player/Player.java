f4597baba002665404fa03a105e60881d2b19b06
huongpham21196 <huongpham21196@gmail.com>
2019-03-21 00:47:24 +0700

    second commit

--- src/game/player/Player.java
+++ src/game/player/Player.java
@@ -6,6 +6,7 @@ import game.Settings;
 import game.physics.BoxCollider;
 
 import java.awt.*;
+import java.util.Set;
 
 public class Player extends GameObject {
 
@@ -24,6 +25,25 @@ public class Player extends GameObject {
     public void run() {
         super.run();
         playerMove();
+        playerLimit();
+    }
+
+    private void playerLimit() {
+        double offsetWidth = anchor.x * Settings.PLAYER_WIDTH;
+        double offsetHeight = anchor.x * Settings.PLAYER_HEIGHT;
+
+        if(position.x < offsetWidth) {
+            position.x = offsetWidth;
+        }
+        if(position.x > Settings.BACKGROUND_WIDTH/2 - offsetWidth) {
+            position.x = Settings.BACKGROUND_WIDTH/2 - offsetWidth;
+        }
+        if(position.y < offsetHeight) {
+            position.y = offsetHeight;
+        }
+        if(position.y > Settings.BACKGROUND_HEIGHT - offsetHeight) {
+            position.y = Settings.BACKGROUND_HEIGHT - offsetHeight;
+        }
     }
 
     private void playerMove() {
--- src/game/player/PlayerRenderer.java
+++ src/game/player/PlayerRenderer.java
@@ -10,31 +10,40 @@ import tklibs.SpriteUtils;
 import java.awt.*;
 
 public class PlayerRenderer extends Renderer {
-    AnimationRenderer upAnimation;
-    AnimationRenderer headAnimation;
-    AnimationRenderer downAnimation;
+//    AnimationRenderer upAnimation;
+//    AnimationRenderer headAnimation;
+//    AnimationRenderer downAnimation;
+
+    AnimationRenderer birdAnimation;
+
+    SingleImageRenderer birdSingle;
 
     public PlayerRenderer() {
-        upAnimation = new AnimationRenderer("assets/sprites/bluebird-upflap", 10);
-        headAnimation = new AnimationRenderer("assets/sprites/bluebird-midflap", 10);
-        downAnimation = new AnimationRenderer("assets/sprites/bluebird-downflap", 10);
+        birdAnimation = new AnimationRenderer("assets/sprites/yellowbird", 20);
+
+        birdSingle = new SingleImageRenderer(SpriteUtils.loadImage("assets/sprites/yellowbird/yellowbird-midflap"));
+
+//        upAnimation = new SingleImageRenderer(SpriteUtils.loadImage("assets/sprites/yellowbird/yellowbird-upflap"));
+//        headAnimation = new AnimationRenderer("assets/sprites/yellowbird/yellowbird-midflap", 10);
+//        downAnimation = new AnimationRenderer("assets/sprites/yellowbird/yellowbird-downflap", 10);
     }
 
     int renderCount;
     @Override
     public void render(Graphics g, GameObject master) {
         Player player = (Player) master;
+        birdAnimation.render(g,master);
         renderCount++;
-        if(renderCount % 2 == 0) {
-            return;
-        }
-        if(master.velocity.y < 0) {
-            downAnimation.render(g, master);
-        } else if(master.velocity.y > 0) {
-            upAnimation.render(g, master);
-        } else {
-            headAnimation.render(g, master);
-        }
+//        if(renderCount % 2 == 0) {
+//            return;
+//        }
+//        if(master.velocity.y < 0) {
+//            downAnimation.render(g, master);
+//        } else if(master.velocity.y > 0) {
+//            upAnimation.render(g, master);
+//        } else {
+//            headAnimation.render(g, master);
+//        }
     }
 
 //    @Override
